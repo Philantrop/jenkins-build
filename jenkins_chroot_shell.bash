c120e1c60d8701c41997f185e9309b606ca3faa5
Bo Ørsted Andresen <zlin@exherbo.org>
2015-04-26 22:25:02 +0200

    Add script for running a custom set of commands defined in a heredoc.

---
+++ jenkins_chroot_shell.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CHROOT=${1}
+WORKSPACE=${2}
+JOB_NAME=${3}
+DEBUG=${4:-0}
+if [[ $# -gt 4 ]]; then
+    shift 4
+    NSPAWN_ARGS="$@"
+fi
+
+EXECUTE=$(cat)
+
+[[ ${DEBUG} -eq 0 ]] && sudo rsync -aHx --exclude="*~" --force --delete --delete-excluded /srv/jenkins/amd64_base/amd64/* "${CHROOT}" || echo "rsync failed"
+
+sudo /usr/bin/systemd-nspawn \
+    "${NSPAWN_ARGS[@]}" \
+    --bind="${WORKSPACE}":/var/db/paludis/gerrit/${JOB_NAME} \
+    -D "${CHROOT}" /bin/bash -c "${EXECUTE}"
+
