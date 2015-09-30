ef0d5e2ffa16ea8de57beda389ef19cc2d96b18e
Bo Ørsted Andresen <zlin@exherbo.org>
2015-09-30 13:19:31 +0200

    Use a separate :base paludis environment with no pbin repo configured.
    Pbins can come from submitted builds that failed at preinst / postinst time, or
    that succeeded but still got abandoned. The base chroot should not use either.
    
    Really this base env should just need ::arbor so new repositories should not
    need to be added here. CAVE_RESOLVE_OPTIONS is used to enable --via-pbin and
    therefore needs to be ignored here.

--- update_base.bash
+++ update_base.bash
@@ -5,28 +5,32 @@ source /etc/profile
 stty columns 80 rows 40
 
 # Lower pbin importance
-sed -i -e "s:^\(importance =\).*:\1 -100:" /etc/paludis/repositories/pbin.conf
+#sed -i -e "s:^\(importance =\).*:\1 -100:" /etc/paludis/repositories/pbin.conf
 
 # Turn off tests for the update
 sed -i -e "/\*\/\* build_options:/s:\(recommended_tests\):-\1:" /etc/paludis/options.conf
 
+export CAVE_RESOLVE_OPTIONS=
+
+cave -E :base sync
+
 # Update things
-PALUDIS_DO_NOTHING_SANDBOXY=y cave resolve world -c  --recommendations ignore  --suggestions ignore --purge '*/*' -x
+PALUDIS_DO_NOTHING_SANDBOXY=y cave -E :base resolve world -c  --recommendations ignore  --suggestions ignore --purge '*/*' -x
 rc=$?
 
 # Fix linkage
-PALUDIS_DO_NOTHING_SANDBOXY=y cave fix-linkage -x
+PALUDIS_DO_NOTHING_SANDBOXY=y cave -E :base fix-linkage -x
 rc=$?
 
 # Fix cache
-PALUDIS_DO_NOTHING_SANDBOXY=y cave fix-cache
+PALUDIS_DO_NOTHING_SANDBOXY=y cave -E :base fix-cache
 rc=$?
 
 # Re-enable tests
 sed -i -e "/\*\/\* build_options:/s:-\(recommended_tests\):\1:" /etc/paludis/options.conf 
 
 # Raise pbin importance
-sed -i -e "s:^\(importance =\).*:\1 0:" /etc/paludis/repositories/pbin.conf
+#sed -i -e "s:^\(importance =\).*:\1 0:" /etc/paludis/repositories/pbin.conf
 
 eclectic --no-color config list |
     awk '$1~/^\[[[:digit:]]\]$/{print $2}' |
